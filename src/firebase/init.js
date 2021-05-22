5843a647e0c13991b4de67e8deda4bb3c023fd5c
sinapples <Josh.lopez.is@gmail.com>
2021-05-22 01:07:03 -0400

    hello

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,13 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
-  authDomain: 'bento-starter.firebaseapp.com',
-  databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
-  storageBucket: 'bento-starter.appspot.com',
-  messagingSenderId: '458464977217'
+  apiKey: 'AIzaSyBBlGECHZK3d_dF-pvVW9vbnMB7OL3uio0',
+  authDomain: 'cafe-boba.firebaseapp.com',
+  projectId: 'cafe-boba',
+  storageBucket: 'cafe-boba.appspot.com',
+  messagingSenderId: '365268196688',
+  appId: '1:365268196688:web:88a92a59351109f7266055',
+  measurementId: 'G-MQNSCP11F3'
 }
 
 firebase.initializeApp(config)
